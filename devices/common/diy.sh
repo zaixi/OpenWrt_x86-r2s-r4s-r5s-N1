66bebcf8b83c03c429bac7d163167a33f1478afb
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-02-28 23:23:36 +0800

    update

--- devices/common/diy.sh
+++ devices/common/diy.sh
@@ -57,7 +57,7 @@ git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/gener
 rm -rf target/linux/generic/hack-6.6/929-Revert-genetlink* target/linux/generic/hack-6.6/767-net-phy-realtek-add-led*
 wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-6.6/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-6.6/
 
-wget -N https://patch-diff.githubusercontent.com/raw/openwrt/openwrt/pull/16414.patch -P devices/common/patches/
+wget -N https://github.com/kiddin9/openwrt1/commit/b0ca5138c0fc0e528a4a06437eedf136e75af5e9.patch -P devices/common/patches/
 
 # find target/linux/x86 -name "config*" -exec bash -c 'cat kernel.conf >> "{}"' \;
 sed -i 's/max_requests 3/max_requests 20/g' package/network/services/uhttpd/files/uhttpd.config
